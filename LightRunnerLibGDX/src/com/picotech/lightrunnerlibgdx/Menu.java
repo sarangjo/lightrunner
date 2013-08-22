3c964499cfc6de639db35a4fac34a17760a6846e
Atticus Liu <atticusacl@gmail.com>
2013-08-22 00:24:35 -0700

    Updated main screen and credits. Still need to activate them. There's a problem with too many squares showing up on start screen, creates lag. If possible, logo should be made larger and lowered. Functionality needs to be established.

--- LightRunnerLibGDX/src/com/picotech/lightrunnerlibgdx/Menu.java
+++ LightRunnerLibGDX/src/com/picotech/lightrunnerlibgdx/Menu.java
@@ -16,7 +16,7 @@ public class Menu extends Sprite2 {
 	public BitmapFont bf;
 	
 	// MainMenu
-	public Rectangle playButton, instructionsButton, statisticsButton, optionsButton, quitButton;
+	public Rectangle playButton, instructionsButton, statisticsButton, optionsButton, creditsButton, quitButton;
 	// Pause
 	public Rectangle resumeButton, restartButton, backMainButton;
 	// Options
@@ -29,11 +29,12 @@ public class Menu extends Sprite2 {
 	public Menu() {
 		super(new Vector2(0,0), 1280, 720);
 		// Initializes the rectangular buttons to be a particular x, y, width, height
-		playButton = new Rectangle(390, 400, 500, 60);
-		instructionsButton = new Rectangle(390, 310, 500, 60);
-		statisticsButton = new Rectangle(390, 220, 500, 60);
-		optionsButton = new Rectangle(390, 130, 500, 60);
-		quitButton = new Rectangle(390, 40, 500, 60);
+		playButton = new Rectangle(440, 160, 400, 400); 
+		instructionsButton = new Rectangle(1050, 400, 200, 80);
+		statisticsButton = new Rectangle(1050, 310, 200, 80);
+		optionsButton = new Rectangle(1050, 220, 200, 80);
+		creditsButton = new Rectangle(1050, 130, 200, 80);
+		quitButton = new Rectangle(1050, 40, 200, 80);
 
 		// Pause
 		resumeButton = new Rectangle(800, 460, 400, 100);
@@ -61,14 +62,33 @@ public class Menu extends Sprite2 {
 		switch (menuState)
 		{
 			case CREDITS:
-				break;
+				//also needs a back button
+				//this is completely spitball
+				//white font on black background to make our names pop
+				
+				//LightRunner logo here
+				batch.begin();
+				Assets.drawByPixels(batch, blackScreen, Color.BLACK);
+				bf.setColor(Color.WHITE);
+				bf.draw(batch, "Cameron Akker", 540, 120);
+				bf.draw(batch, "Daniel Fang", 540, 200);
+				bf.draw(batch, "Sarang Joshi", 540, 280);
+				bf.draw(batch, "Adarsh Karnati", 540, 360);
+				bf.draw(batch,  "Atticus Liu", 540, 480);
+				bf.draw(batch, "Special thanks to StudentRND", 480, 580);
+				batch.end();
+				//break;
 			case INSTRUCTIONS:
+				//two-fold: three plot .png's come here
+				//then put the instruction .png's 
+				//will take place as a sequence
 				break;
 			case MAIN:
 				Assets.drawByPixels(batch, playButton, Color.WHITE);
 				Assets.drawByPixels(batch, instructionsButton, Color.WHITE);
 				Assets.drawByPixels(batch, statisticsButton, Color.WHITE);
 				Assets.drawByPixels(batch, optionsButton, Color.WHITE);
+				Assets.drawByPixels(batch, creditsButton, Color.WHITE);
 				Assets.drawByPixels(batch, quitButton, Color.WHITE);
 				
 				batch.begin();
@@ -78,11 +98,12 @@ public class Menu extends Sprite2 {
 				// 	Text
 				batch.begin();
 				bf.setColor(Color.BLACK);
-				bf.draw(batch, "Play", 610, getMainY(playButton)); // playButton.y + playButton.height - 15);
-				bf.draw(batch, "Instructions", 575, getMainY(instructionsButton));
-				bf.draw(batch, "Statistics", 583, getMainY(statisticsButton));
-				bf.draw(batch, "Options", 593, getMainY(optionsButton));
-				bf.draw(batch, "Quit", 610, getMainY(quitButton));
+				bf.draw(batch, "Play", 500, getMainY(playButton)); // playButton.y + playButton.height - 15);
+				bf.draw(batch, "Instructions", 1080, getMainY(instructionsButton));
+				bf.draw(batch, "Statistics", 1090, getMainY(statisticsButton));
+				bf.draw(batch, "Options", 1095, getMainY(optionsButton));
+				bf.draw(batch, "Credits", 1100, getMainY(creditsButton));
+				bf.draw(batch, "Quit", 1120, getMainY(quitButton));
 
 				batch.end();			
 				break;
@@ -115,7 +136,7 @@ public class Menu extends Sprite2 {
 	
 	public float getMainY(Rectangle r)
 	{
-		return r.y + (.75f*r.height);	
+		return r.y + (.65f*r.height);	
 	}
 	public float getPauseY(Rectangle r)
 	{
